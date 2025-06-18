b7ac423c20d01f39fc5c9657107f84e56f96f324
Laksh Bharani <laksh.bharani@gmail.com>
2025-06-18 13:30:42 -0700

    Initial Commit

---
+++ nux/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  nux
+//
+//  Created by Laksh Bharani on 6/18/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ nux/nuxApp.swift
@@ -0,0 +1,17 @@
+//
+//  nuxApp.swift
+//  nux
+//
+//  Created by Laksh Bharani on 6/18/25.
+//
+
+import SwiftUI
+
+@main
+struct nuxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
